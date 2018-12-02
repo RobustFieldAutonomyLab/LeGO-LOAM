bf2c0c60ff67a6c0c5278f5b24413147ea58e21e
Tixiao Shan <tixiao.shan@gmail.com>
2018-12-02 12:04:37 -0500

    Update utility.h

--- LeGO-LOAM/include/utility.h
+++ LeGO-LOAM/include/utility.h
@@ -50,12 +50,21 @@ using namespace std;
 
 typedef pcl::PointXYZI  PointType;
 
+// VLP-16
 extern const int N_SCAN = 16;
 extern const int Horizon_SCAN = 1800;
 extern const float ang_res_x = 0.2;
 extern const float ang_res_y = 2.0;
 extern const float ang_bottom = 15.0+0.1;
 extern const int groundScanInd = 7;
+
+// Ouster OS1-64
+// extern const int N_SCAN = 64;
+// extern const int Horizon_SCAN = 1024;
+// extern const float ang_res_x = 360.0/float(Horizon_SCAN-1);
+// extern const float ang_res_y = 33.2/float(N_SCAN-1);
+// extern const float ang_bottom = 16.6+0.1;
+// extern const int groundScanInd = 15;
 
 extern const bool loopClosureEnableFlag = false;
 extern const double mappingProcessInterval = 0.3;
