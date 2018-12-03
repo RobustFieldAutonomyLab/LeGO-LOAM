5555f93fa020d2d720b7557dc3262aa24e16ba61
Tixiao Shan <tixiao.shan@gmail.com>
2018-12-02 20:01:39 -0500

    Add Ouster Lidar (OS-1-64) Configuration

--- LeGO-LOAM/include/utility.h
+++ LeGO-LOAM/include/utility.h
@@ -61,7 +61,7 @@ extern const int groundScanInd = 7;
 // Ouster OS1-64
 // extern const int N_SCAN = 64;
 // extern const int Horizon_SCAN = 1024;
-// extern const float ang_res_x = 360.0/float(Horizon_SCAN-1);
+// extern const float ang_res_x = 360.0/float(Horizon_SCAN);
 // extern const float ang_res_y = 33.2/float(N_SCAN-1);
 // extern const float ang_bottom = 16.6+0.1;
 // extern const int groundScanInd = 15;
