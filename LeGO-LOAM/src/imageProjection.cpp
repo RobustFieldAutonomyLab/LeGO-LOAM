1d09538708e511bfcad574ac79970f5781346898
Tixiao Shan <tixiao.shan@gmail.com>
2018-12-02 20:02:45 -0500

    range image projection minor fix

--- LeGO-LOAM/src/imageProjection.cpp
+++ LeGO-LOAM/src/imageProjection.cpp
@@ -201,12 +201,9 @@ public:
 
             horizonAngle = atan2(thisPoint.x, thisPoint.y) * 180 / M_PI;
 
-            if (horizonAngle <= -90)
-                columnIdn = -int(horizonAngle / ang_res_x) - int(Horizon_SCAN/4); 
-            else if (horizonAngle >= 0)
-                columnIdn = -int(horizonAngle / ang_res_x) + int(Horizon_SCAN/4*3);
-            else
-                columnIdn = int(Horizon_SCAN/4*3) - int(horizonAngle / ang_res_x);
+            columnIdn = -round((horizonAngle-90.0)/ang_res_x) + Horizon_SCAN/2;
+            if (columnIdn >= Horizon_SCAN)
+                columnIdn -= Horizon_SCAN;
 
             if (columnIdn < 0 || columnIdn >= Horizon_SCAN)
                 continue;
