a58ced652d46741a34a9bf646f79c74f99b7178f
Davide Faconti <daf@blue-ocean-robotics.com>
2019-06-06 17:14:55 +0200

    whitespaces added accidentally?

--- LeGO-LOAM/src/featureAssociation.cpp
+++ LeGO-LOAM/src/featureAssociation.cpp
@@ -754,47 +754,6 @@ public:
 	    }
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     void TransformToStart(PointType const * const pi, PointType * const po)
     {
         float s = 10 * (pi->intensity - int(pi->intensity));
