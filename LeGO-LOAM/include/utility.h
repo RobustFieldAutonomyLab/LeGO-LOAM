898f2dfbb304fbd8c6b6b2d216bfec99ef1bb225
Tixiao Shan <tixiao.shan@gmail.com>
2019-06-11 13:34:59 -0400

    fix save pcd bug, thanks to @MikeZhu92

--- LeGO-LOAM/include/utility.h
+++ LeGO-LOAM/include/utility.h
@@ -53,6 +53,9 @@ typedef pcl::PointXYZI  PointType;
 extern const string pointCloudTopic = "/velodyne_points";
 extern const string imuTopic = "/imu/data";
 
+// Save pcd
+extern const string fileDirectory = "/tmp/";
+
 // VLP-16
 extern const int N_SCAN = 16;
 extern const int Horizon_SCAN = 1800;
